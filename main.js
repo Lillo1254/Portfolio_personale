8ea6ebe5163e9da70d765870f9643b1bacc5be19
alessandro lucia <alessandrolucia75@gmail.com>
2025-08-29 16:44:01 +0200

    aggiunte funzionalita js

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-let first = document.querySelector('#card1');
+let benvenuto = localStorage.getItem("nome");
+
+if (benvenuto) {
+    alert("Bentornata/o " + benvenuto.toUpperCase() + " il local storage si svuota ogni 10 minuti con un set interval di 600000ms");
+} else {
+    benvenuto = prompt("Inserisci il tuo nome");
+    localStorage.setItem("nome", benvenuto);
+}
+const clearef = setInterval(() => {
+    localStorage.clear();
+    console.log("LocalStorage cancellato!");
+}, 600000);
+
+let title = document.querySelector('#title')
+
+if (benvenuto) {
+    title.innerText = "Piacere di conoscerti " + benvenuto + " !";
+    } else if (benvenuto === null) {
+    title.innerText = "Piacere di conoscerti";
+}
+
+const color = document.querySelector('#title');
+
+setInterval(() => {
+    color.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+}, 1500)
+
+let fine = document.querySelector("#fine");
+if (benvenuto) {
+    fine.innerText = "P.s. Gentile " + benvenuto + " cliccando sull \' immagine del progetto verra reindirizzato alla pagina github con la struttura del codice. Grazie per la visita !";
+}
